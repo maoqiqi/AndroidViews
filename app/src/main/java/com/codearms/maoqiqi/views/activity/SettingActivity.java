2c20a2b5c7e5c98e4def61d8ba265ca57c9cd376
maoqiqi <fengqi.mao.march@gmail.com>
2018-08-24 15:47:18 +0800

    Update settings interface, demonstrate CheckedTextView, SwitchCompat, ToggleButton to achieve the setting effect.

--- app/src/main/java/com/codearms/maoqiqi/views/activity/SettingActivity.java
+++ app/src/main/java/com/codearms/maoqiqi/views/activity/SettingActivity.java
@@ -5,8 +5,10 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.CheckedTextView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.codearms.maoqiqi.views.BaseActivity;
 import com.codearms.maoqiqi.views.R;
@@ -17,6 +19,13 @@ import com.codearms.maoqiqi.views.R;
  * Date: 2018/8/23 17:50
  */
 public class SettingActivity extends BaseActivity implements View.OnClickListener {
+
+    private CheckedTextView rememberPassword1;
+    private CheckedTextView rememberPassword2;
+    private SwitchCompat rememberPassword3;
+    private SwitchCompat rememberPassword4;
+    private RelativeLayout rememberPassword5;
+    private ToggleButton toggleButton;
 
     private RelativeLayout rlSystemAnimation;
     private TextView tvSystemAnimationTitle;
@@ -40,6 +49,13 @@ public class SettingActivity extends BaseActivity implements View.OnClickListene
         toolbar.setTitle(R.string.setting);
         setSupportActionBar(toolbar);
 
+        rememberPassword1 = findViewById(R.id.remember_password_1);
+        rememberPassword2 = findViewById(R.id.remember_password_2);
+        rememberPassword3 = findViewById(R.id.remember_password_3);
+        rememberPassword4 = findViewById(R.id.remember_password_4);
+        rememberPassword5 = findViewById(R.id.remember_password_5);
+        toggleButton = findViewById(R.id.toggle_button);
+
         rlSystemAnimation = findViewById(R.id.rl_system_animation);
         tvSystemAnimationTitle = findViewById(R.id.tv_system_animation_title);
         tvSystemAnimationContent = findViewById(R.id.tv_system_animation_content);
@@ -52,6 +68,10 @@ public class SettingActivity extends BaseActivity implements View.OnClickListene
         tvLauncherPageRandomTitle = findViewById(R.id.tv_launcher_page_random_title);
         tvLauncherPageRandomContent = findViewById(R.id.tv_launcher_page_random_content);
         scLauncherPageRandom = findViewById(R.id.sc_launcher_page_random);
+
+        rememberPassword1.setOnClickListener(this);
+        rememberPassword2.setOnClickListener(this);
+        rememberPassword5.setOnClickListener(this);
 
         rlSystemAnimation.setOnClickListener(this);
         rlLauncherPage.setOnClickListener(this);
@@ -67,6 +87,15 @@ public class SettingActivity extends BaseActivity implements View.OnClickListene
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.remember_password_1:
+                rememberPassword1.toggle();
+                break;
+            case R.id.remember_password_2:
+                rememberPassword2.toggle();
+                break;
+            case R.id.remember_password_5:
+                toggleButton.toggle();
+                break;
             case R.id.rl_system_animation:
                 scSystemAnimation.toggle();
                 break;
@@ -85,7 +114,7 @@ public class SettingActivity extends BaseActivity implements View.OnClickListene
         if (scLauncherPage.isChecked()) {
             tvLauncherPageContent.setText(R.string.launcher_page_content_1);
             rlLauncherPageRandom.setClickable(true);
-            tvLauncherPageRandomTitle.setTextColor(ContextCompat.getColor(this, R.color.color_primary));
+            tvLauncherPageRandomTitle.setTextColor(ContextCompat.getColor(this, R.color.text_color_primary));
         } else {
             tvLauncherPageContent.setText(R.string.launcher_page_content_2);
             rlLauncherPageRandom.setClickable(false);
